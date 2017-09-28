685de20c1a3b4321b819ad8a42a3fbd502376d45
Gulzira <vip.chopi@gmail.com>
2017-09-29 01:36:13 +0600

    Коммит жарияланды.

--- src/kz/kaznitu/lessons/Hello.java
+++ src/kz/kaznitu/lessons/Hello.java
@@ -3,5 +3,7 @@ package kz.kaznitu.lessons;
 public class Hello {
     public static void main (String [] args){
         System.out.println("Hello world!");
+        System.out.println("Бұл зертханалық жұмыстың мақсаты:");
+        System.out.println("GitHup-пен жұмыс жасап үйрену");
     }
 }
